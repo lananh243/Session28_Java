d5525d3b370ec43dba3e8f95e36f9a17a02b0bb8
lananh243 <lananhchuche21092005@gmail.com>
2025-04-09 10:24:30 +0700

    bt-ss28

--- src/Bai9.java
+++ src/Bai9.java
@@ -14,17 +14,31 @@ public class Bai9 {
         CallableStatement callStmt = null;
         try {
             conn = ConnectionDB.openConnection();
+            conn.setAutoCommit(false);
+
             String sql = ("{call placebid(?, ?, ?)}");
             callStmt = conn.prepareCall(sql);
             callStmt.setInt(1, auction_id);
             callStmt.setInt(2, user_id);
             callStmt.setDouble(3, bid_amount);
             callStmt.executeUpdate();
-            System.out.println("Bid placed successfully!");
+            System.out.println("Đã đặt giá thầu thành công!");
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("SQL lỗi: " + e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
+                callStmt.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
